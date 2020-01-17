fa8ee86c845a40b692faddced62da16981b75dd7
Mori Morou <mori_morou@yahoo.com>
2020-01-18 01:07:18 +0300

    HW dome

---
+++ src/main/java/org/example/HomeWork_Three/Ball.java
@@ -0,0 +1,28 @@
+package org.example.HomeWork_Three;
+
+public class Ball {
+    private int kicks = 0;
+    private static Ball instance = new Ball();
+    private String side = "";
+
+    private Ball() {
+    }
+
+    static Ball getBall() {
+        return instance;
+    }
+
+    synchronized void kickBall(String playerName) {
+        kicks++;
+        side = playerName;
+        System.out.println(kicks + " " + side);
+    }
+
+    String getSide() {
+        return side;
+    }
+
+    boolean isInGame() {
+        return (kicks < 15);
+    }
+}
